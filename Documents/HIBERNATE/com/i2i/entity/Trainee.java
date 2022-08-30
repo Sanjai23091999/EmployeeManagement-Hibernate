f8e83b6cada62113acf5ae8f80572102f0c32fe0
sanjai23091999 <sanjai.king@ideas2it.com>
2022-08-30 12:23:38 +0530

    completed

---
+++ Documents/HIBERNATE/com/i2i/entity/Trainee.java
@@ -0,0 +1,70 @@
+package com.i2i.entity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.List;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQuery;
+import javax.persistence.NamedQueries;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import javax.persistence.CascadeType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+
+@NamedQueries(
+    {
+    @NamedQuery(  
+        name = "retriveTraineeById",  
+        query = "from Trainee trainee  where trainee.employeeId = :employeeId AND trainee.isActiveEmployee = :isActiveEmployee" 
+    ), 
+    @NamedQuery(  
+        name = "retriveAllTrainees",  
+        query = "from Trainee trainee where trainee.isActiveEmployee = :status"  
+    )
+    }
+)
+
+@Entity
+public class Trainee extends Employee{
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    private int TraineeId;
+
+    @ManyToMany(cascade = CascadeType.ALL,mappedBy="trainees") 
+    private List<Trainer> trainers;
+        
+    public List<Trainer> getTrainers(){
+	return trainers;
+    }
+                                        
+    public void setTrainers(List<Trainer> trainers) {
+	this.trainers = trainers;
+    }
+
+}
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
